42da73859efb2bc129bf980716d5cdd87c8a9887
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-09-18 18:50:21 +0530

    feat: add User model

--- src/config/settings.py
+++ src/config/settings.py
@@ -117,3 +117,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication
+
+AUTH_USER_MODEL = "users.User"
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    email = models.EmailField("email", unique=True)
+    name = models.CharField("name", max_length=256, blank=True, null=True)
