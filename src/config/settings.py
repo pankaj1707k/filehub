66e91ba41948a222a5010defe580fb9d9be62ac9
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-09-18 16:48:32 +0530

    fix: users app registration in INSTALLED_APPS

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users.config.UsersConfig",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
