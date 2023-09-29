960fbfe9f11b175b3692b917b0b056d33759e08f
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-09-29 23:32:11 +0530

    feat: add tracer code for templates and views
    Add basic html templates and views to render them

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,8 +4,9 @@ URL configuration for config project.
 The `urlpatterns` list routes URLs to views.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("users.urls")),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import generic
 
-# Create your views here.
+
+class HomeView(generic.TemplateView):
+    template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("dashboard")
+        return super().get(request, *args, **kwargs)
+
+
+class LoginView(generic.TemplateView):
+    template_name = "login.html"
