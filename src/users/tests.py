898fb0a51ca46d62ad556205d65bb0bcdede4f24
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-10-01 23:58:33 +0530

    test|refactor: add tests for login template and redirect with refactoring
    Refactor to use setUpClass() instead of setUp() to reduce irrelevant redundant operations

--- src/users/tests.py
+++ src/users/tests.py
@@ -8,13 +8,17 @@ User = get_user_model()
 
 class UserAuthTest(TestCase):
     """
-    Tests for `AuthenticationBackend`.
+    Tests for user authentication.
     """
 
-    def setUp(self):
-        self.testuser = User.objects.create(username="testuser0", email="tu0@test.com")
-        self.testuser.set_password("testing@321")
-        self.testuser.save()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.testuser = User.objects.create(username="testuser0", email="tu0@test.com")
+        cls.testuser.set_password("testing@321")
+        cls.testuser.save()
+        cls.login_template = "login.html"
+        cls.login_url = reverse(settings.LOGIN_URL)
 
     def test_username_login(self):
         self.assertFalse(get_user(self.client).is_authenticated)
@@ -26,9 +30,22 @@ class UserAuthTest(TestCase):
         self.client.login(username="tu0@test.com", password="testing@321")
         self.assertTrue(get_user(self.client).is_authenticated)
 
+    def test_render_template(self):
+        response = self.client.get(self.login_url)
+        self.assertTemplateUsed(response, self.login_template)
+
+    def test_login_success_redirect(self):
+        login_data = {"username": "testuser0", "password": "testing@321"}
+        response = self.client.post(self.login_url, login_data, follow=True)
+        self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
+
     def tearDown(self):
         self.client.logout()
-        self.testuser.delete()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.testuser.delete()
 
 
 class UserRegisterTest(TestCase):
@@ -36,10 +53,12 @@ class UserRegisterTest(TestCase):
     Tests for user registration.
     """
 
-    def setUp(self):
-        self.url = reverse("register")
-        self.template_name = "register.html"
-        self.testuser = {
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.register_url = reverse("register")
+        cls.register_template = "register.html"
+        cls.testuser = {
             "username": "testuser",
             "email": "tu@test.com",
             "password1": "asd@lkj123",
@@ -47,17 +66,17 @@ class UserRegisterTest(TestCase):
         }
 
     def test_render_template(self):
-        response = self.client.get(self.url)
-        self.assertTemplateUsed(response, self.template_name)
+        response = self.client.get(self.register_url)
+        self.assertTemplateUsed(response, self.register_template)
 
     def test_register_success(self):
-        response = self.client.post(self.url, self.testuser)
+        response = self.client.post(self.register_url, self.testuser)
         self.assertEqual(response.status_code, 302)
-        self.assertTemplateNotUsed(response, self.template_name)
+        self.assertTemplateNotUsed(response, self.register_template)
         self.assertTrue(
             User.objects.filter(username=self.testuser["username"]).exists()
         )
 
     def test_register_success_redirect(self):
-        response = self.client.post(self.url, self.testuser, follow=True)
+        response = self.client.post(self.register_url, self.testuser, follow=True)
         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
