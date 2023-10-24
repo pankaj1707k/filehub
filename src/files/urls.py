a2f220b0257dfc5303fc355f1017ff975a2d42ad
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-10-24 11:47:33 +0530

    refactor: replace hardcoded url with template tag in dashboard

--- src/files/urls.py
+++ src/files/urls.py
@@ -5,7 +5,7 @@ from files import views
 urlpatterns = [
     path("get-signed-url/", views.SignedURLView.as_view(), name="signed_url"),
     path("f/new/", views.FileCreateView.as_view(), name="create_file"),
-    path("d/children/", views.FileDirListView.as_view(), name="list_children"),
+    path("d/content/", views.FileDirListView.as_view(), name="list_contents"),
     path("d/new/", views.DirectoryCreateView.as_view(), name="create_dir"),
     path("d/<uuid:id>/", views.DirectoryView.as_view(), name="dir_contents"),
     path("d/<uuid:id>/update/", views.DirectoryUpdateView.as_view(), name="update_dir"),
